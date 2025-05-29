a4b8c81646c1bdb72b13f2678958b1ae65417dcc
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-05-29 13:09:30 +0100

    fix: add cart_tools template tag library for subtotal calculations

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-from django.views.decorators.http import require_POST
-from django.http import JsonResponse
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from cart.contexts import cart_contents
-from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+from cart.inventory import cart_contents
 
 import stripe
 import json
