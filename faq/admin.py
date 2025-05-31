a36c7d470977a26353f96b2a6926b1211500718d
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-05-31 22:45:32 +0100

    feat: add admin.py for faq page

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import FAQ
 
-# Register your models here.
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'created_at')
+    search_fields = ('question', 'answer')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # only set user on creation
+            obj.user = request.user
+        super().save_model(request, obj, form, change)
