8c75dcfa70006af85ddc3372870e34d1f434c275
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-05-31 22:11:02 +0100

    feat: add to faq section, views.py, urls.py and admin.py

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import FAQ
 
-# Create your views here.
+class FAQListView(ListView):
+    model = FAQ
+    template_name = 'faq/faq_list.html'
+    context_object_name = 'faqs'
