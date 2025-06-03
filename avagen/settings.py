cc5be3fa02b2040c3f8bb23905f6e72773adc677
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-06-03 15:06:50 +0100

    implement verification email email settings with django allauth, add email_backend.py to fix incompability issue with python 3.12 and the latest Django

--- avagen/settings.py
+++ avagen/settings.py
@@ -122,10 +122,11 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'avagen.email_backend.CustomEmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'polymath.verse@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'nxgx usxh jjsz xjup')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', 'polymath.verse@gmail.com')
