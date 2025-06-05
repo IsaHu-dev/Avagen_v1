c02d5f82067ea7ce12a332f69e0cebb35eb14c0c
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-06-05 09:36:19 +0100

    refactor: move Review model to a separate reviews app
    - Created a new reviews app and moved the Review model there.Created a custom model for the reviews app

--- products/models.py
+++ products/models.py
@@ -60,14 +60,4 @@ class Product(models.Model):
     # String representation of the product
     def __str__(self):
         return self.name
-    
-class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
-    name = models.CharField(max_length=100)
-    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])  # 1–5 stars
-    comment = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.product.name} - {self.rating} Stars by {self.name}"
 
