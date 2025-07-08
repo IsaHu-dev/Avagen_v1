38b57c94c0fa9693766fa9e1b90db424b916c333
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-07-08 23:01:35 +0100

    Remove user field from FAQ admin and model

--- faq/admin.py
+++ faq/admin.py
@@ -6,8 +6,4 @@ from .models import FAQ
 class FAQAdmin(admin.ModelAdmin):
     list_display = ('question', 'created_at')
     search_fields = ('question', 'answer')
-
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:  # only set user on creation
-            obj.user = request.user
-        super().save_model(request, obj, form, change)
+    exclude = ('user',)
