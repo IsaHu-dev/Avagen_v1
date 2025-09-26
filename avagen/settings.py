608fc9f82db75cd4bcd3924ace1b77f4e313449f
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-09-26 15:14:25 +0100

    set debug to false

--- avagen/settings.py
+++ avagen/settings.py
@@ -53,7 +53,7 @@ else:
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
